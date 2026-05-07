59696764291fe454cd4cffcfea1f9e9b946a029b
Joshua Murphy <2576504+murphyjt@users.noreply.github.com>
2026-05-07 17:10:22 -0400

    Initial Commit

---
+++ WaveBird/WaveBirdApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaveBirdApp.swift
+//  WaveBird
+//
+//  Created by Joshua Murphy on 5/7/26.
+//
+
+import SwiftUI
+
+@main
+struct WaveBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WaveBirdTests/WaveBirdTests.swift
@@ -0,0 +1,19 @@
+//
+//  WaveBirdTests.swift
+//  WaveBirdTests
+//
+//  Created by Joshua Murphy on 5/7/26.
+//
+
+import Testing
+@testable import WaveBird
+
+struct WaveBirdTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
